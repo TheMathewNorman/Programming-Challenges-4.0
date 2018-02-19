872629063178c0a5f2861146f5ae1043dbade58e
Mathew Norman <spam@matnorman.com>
2018-02-20 06:03:10 +1100

    Completed #61 ROT13 in C#

---
+++ 61 - ROT 13/C#/ROT 13/ROT 13/ROT13.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ROT_13
+{
+    partial class ROT13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(436, 26);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(12, 89);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(436, 26);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(12, 44);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(436, 30);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecode.Location = new System.Drawing.Point(12, 121);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(436, 30);
+            this.btnDecode.TabIndex = 4;
+            this.btnDecode.Text = "Decode Output";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // ROT13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 163);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ROT13";
+            this.Text = "ROT 13";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnDecode;
+    }
+}
+
---
+++ 61 - ROT 13/C#/ROT 13/ROT 13/ROT13.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROT_13
+{
+    public partial class ROT13 : Form
+    {
+        public ROT13()
+        {
+            InitializeComponent();
+        }
+
+        private string runCipher(string input)
+        {  
+            string cipherOutput = "";
+            
+            // Iterate through the characters of the input
+            foreach (char c in input.ToCharArray())
+            {
+                if (char.IsUpper(c))
+                {
+                    // If the current character is an uppercase letter, apply the rotation to uppercase letters.
+                    int value = (int)c - 65;
+                    value = ((value + 13) % 26) + 65;
+
+                    // Append the resulting character to the output string.
+                    cipherOutput += Convert.ToChar(value).ToString();
+                }
+                else if (char.IsLower(c))
+                {
+                    // If the current character is an lowercase letter, apply the rotation to lowercase letters.
+                    int value = (int)c - 97;
+                    value = ((value + 13) % 26) + 97;
+
+                    // Append the resulting character to the output string.
+                    cipherOutput += Convert.ToChar(value).ToString();
+                }
+                else
+                {
+                    // No changes for non-alphabetic characters.
+                    cipherOutput += c.ToString();
+                }
+            }
+
+            // Returned the formed string.
+            return cipherOutput;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            // Display the output.
+            txtResult.Text = runCipher(txtInput.Text);
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            // Reverse the cipher
+            txtInput.Text = txtResult.Text;
+            txtResult.Text = runCipher(txtInput.Text);
+        }
+    }
+}
