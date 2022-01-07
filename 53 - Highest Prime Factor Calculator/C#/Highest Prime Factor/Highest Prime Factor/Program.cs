d9a62e63dfdd5562b7d7acf6b7403990fc0fc21e
Mathew Norman <spam@matnorman.com>
2022-01-08 07:10:32 +1100

    Replaced .NET 6 version with .NET 5

---
+++ 53 - Highest Prime Factor Calculator/C#/Highest Prime Factor/Highest Prime Factor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Highest_Prime_Factor
+{
+    internal class Program
+    {
+        static int value = 0;
+
+        /// <summary>
+        /// Returns the highest prime factor of a given input
+        /// </summary>
+        /// <param name="value">Integer value which the highest prime factor is to be found</param>
+        /// <returns></returns>
+        static int getHighestPrimeFactor(int value)
+        {
+            int return_value = 0;
+
+            bool isPrime = true;
+            int currentFactor = 0;
+
+            // Find each factor and check if its prime
+            for (int i = 1; i <= value; i++)
+            {
+                if (value % i == 0)
+                {
+                    currentFactor = i;
+
+                    for (int j = 2; j < currentFactor / 2; j++)
+                        if (value % j == 0) isPrime = false;
+
+                    if (isPrime) return_value = currentFactor;
+                }
+
+                isPrime = true;
+            }
+
+            return return_value;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            try
+            {
+                value = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("Highest prime factor is: {0}", getHighestPrimeFactor(value));
+        }
+    }
+}
